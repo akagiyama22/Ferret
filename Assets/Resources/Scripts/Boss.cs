22f215a237ca67f937cf7ae30bc4b2fd16943973
akagiyama22 <akagiyama22@gmail.com>
2014-11-24 22:31:39 +0900

    初回

---
+++ Assets/Resources/Scripts/Boss.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("DDDDDDDD");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("on trrigger");
+
+		iTweenEvent.GetEvent(gameObject, "hit").Play();
+
+	}
+}
+
