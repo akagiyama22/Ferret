f01aed7edde2475f599de71166f036c199d12539
akagiyama22 <akagiyama22@gmail.com>
2014-11-25 22:36:04 +0900

    クライアントディレクトリ作成

---
+++ Client/Assets/Resources/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ballet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// iTween.Init(gameObject);
+		// iTweenEvent.GetEvent(gameObject, "shot").Play();
+		// tmpEvent.Play();
+		// tmpEvent
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Client/Assets/Resources/Scripts/GUIScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject bullet;
+	[SerializeField]
+	private GameObject bulletsParent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		if (GUI.Button(new Rect(10, 10, 50 , 40), "Shot")) {
+			Debug.Log("Shot!!");
+			// Debug.Log(gameObject;
+			GameObject balletOne = (GameObject)Resources.Load("Prefabs/Bullet");
+			var tmp = Instantiate(balletOne, gameObject.transform.localPosition, Quaternion.identity) as GameObject;
+			tmp.transform.parent = bulletsParent.transform;
+			iTweenEvent.GetEvent(tmp.gameObject, "shot").Play();
+		}
+	}
+}
